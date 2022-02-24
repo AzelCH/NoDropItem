71a2fa8867cd2eabc6679db36924590f274b5415
AzelCH <azelsptn4@gmail.com>
2022-02-24 06:13:45 +0000

    Pm4

---
+++ src/AzelCH/NoDropItem/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AzelCH\NoDropItem;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(): void{
+    $this->saveResource("config.yml");
+    $this->getLogger()->info("§aPlugin Enabled");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onEmote(PlayerDropItemEvent $event){
+    $player = $event->getPlayer();
+    if($this->getConfig()->get("enable") === true){
+      if($this->getConfig()->get("enable-msg") === true){
+        $event->cancel();
+        $player->sendMessage($this->getConfig()->get("dropitem-msg"));
+      }
+    }
+    if($this->getConfig()->get("enable") === true){
+      if($this->getConfig()->get("enable-msg") === false){
+        $event->cancel();
+      }
+    }
+  }
+}
